907919d20ca87c527e5d4458b8261f9b24122708
faithmayer <faithnmayer@gmail.com>
2023-07-29 14:04:02 -0400

    Set up airport and weather APIs

--- pages/api/weather.js
+++ pages/api/weather.js
@@ -1,3 +1,23 @@
-export default function handler(req, res) {
-  res.status(200).json({ text: 'test' });
+import 'dotenv/config'
+
+export default async function handler(req, res) {
+  const body = req.body;
+
+  console.log("body: ", body);
+
+  if (!body.airport) {
+    return res.status(400).json({ data: "Airport not found" });
+  }
+
+  const { latitude_deg, longitude_deg } = await fetch(
+    `https://airportdb.io/api/v1/airport/${body.airport}?apiToken=${process.env.AIRPORT_DB_API_KEY}`
+  )
+    .then((response) => response.json())
+
+  const weather = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude_deg}&lon=${longitude_deg}&appid=${process.env.WEATHER_API_KEY}`
+  )
+    .then((response) => response.json());
+
+  res.status(200).json(weather);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [airport, setAirport] = useState("")
+  const [weather, setWeather] = useState(null)
+
+  async function weatherLookup() {
+    if (!airport) {
+      alert("No airport given :(")
+      return
+    }
+    await fetch("/api/weather", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({airport})
+    }).then((res) => res.json()).then((data) => {
+      setWeather(data)
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,12 +29,25 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Go/No-Go Decision Support Tool
-        </h1>
+        <h1 className={styles.title}>Go/No-Go Decision Support Tool</h1>
 
-        <p className={styles.description}>
-        </p>
+        <input
+          value={airport}
+          onChange={(e) => {
+            setAirport(e.target.value)
+          }}
+        />
+        <button
+          onClick={weatherLookup}
+        >Search</button>
+
+        <p>{JSON.stringify(weather)}</p>
+
+        {/* <form action="/api/weather" method="post">
+          <label for="airport">Airport Iata Code:</label>
+          <input type="text" id="airport" name="airport" />
+          <button type="submit">Submit</button>
+        </form> */}
       </main>
 
       <style jsx>{`
@@ -68,5 +101,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
