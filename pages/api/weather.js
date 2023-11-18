ef4a412f38692f0decce4105a66c78345e8c1f85
faithmayer <faithnmayer@gmail.com>
2023-11-18 17:20:46 -0500

    Determined best runway and compuetd crosswind component

--- pages/api/weather.js
+++ pages/api/weather.js
@@ -21,18 +21,67 @@ export default async function handler(req, res) {
     }
 
     const weather = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude_deg}&lon=${longitude_deg}&appid=${process.env.WEATHER_API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?` +
+      `lat=${latitude_deg}&` +
+      `lon=${longitude_deg}&` +
+      `appid=${process.env.WEATHER_API_KEY}&` +
+      `units=imperial`
     ).then((response) => response.json());
 
     if (weather.message) {
       res.status(weather.cod || 500).json({ message: weather.message });
     }
 
+    // Discard any closed runways
+    airport.runways = airport.runways.filter(runway => runway.closed === "0")
+
+    // Figure out what the headings of the runways are
+    let runways = airport.runways.map(runway => {
+      return [{
+        name: runway.le_ident,
+        length: runway.length_ft,
+        heading: Number(runway.le_ident.slice(0, 2)) * 10
+      }, {
+        name: runway.he_ident,
+        length: runway.length_ft,
+        heading: Number(runway.he_ident.slice(0, 2)) * 10
+      }]
+    }).flat()
+
+    // Pick the best runway based on the wind direction
+    // (whichever the wind direction is closest to)
+    let {deg, speed, gust} = weather.wind
+    let noramlizedDirection = deg > 180 ? deg - 180 : deg + 180
+    let diffs = runways.map(runway => {
+      let normalizedHeading = runway.heading > 180 ? runway.heading - 180 : runway.heading + 180
+      return normalizedHeading - noramlizedDirection
+    })
+    let absDiffs = diffs.map(diff => Math.abs(diff))
+    let desiredIndex = absDiffs.indexOf(Math.min(...absDiffs))
+    let crosswind = diffs[desiredIndex]
+    let desiredRunway = runways[desiredIndex]
+
+    // Convert speed to knots (from MPH)
+    speed = speed / 1.151
+    gust = gust / 1.151
+    // Calculate crosswind component
+    let crosswindComponent = speed * Math.sin(crosswind * (Math.PI / 180))
+
     res.status(200).json({
       weather,
       airport,
+      optimalRunway: {
+        ...desiredRunway,
+        crosswindComponent,
+        crosswindDirection: crosswind > 0 ? "left" : "right",
+        wind: {
+          gustKnots: gust,
+          speedKnots: speed,
+          direction: deg
+        }
+      }
     });
-  } catch {
-    res.status(500).json({ message: "Failed to fetch" });
+  } catch (e) {
+    res.status(500).json({ message: `Failed to compute: ${e}` });
   }
 }
